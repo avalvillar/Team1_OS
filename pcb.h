d2b4a5c969f42b55a7c43d5b96b4d221be8ef3d7
hunterwb <hunterwb@washington.edu>
2016-04-01 13:52:00 -0700

    pcb test, pcb updates

--- pcb.h
+++ pcb.h
@@ -3,7 +3,16 @@
 
 #define PCB_PRIORITY_MAX 15
 
-enum state_type {new, ready, running, interrupted, waiting, halted};
+enum state_type {
+	new = 0, 
+	ready, 
+	running, 
+	interrupted, 
+	waiting, 
+	halted, 
+
+	last_error // invalid type, used for bounds checking
+};
 
 typedef struct pcb {
     unsigned long pid;        // process ID #, a unique number
@@ -16,16 +25,16 @@ typedef PCB * PCB_p;
 
 PCB_p PCB_construct(void); // returns a pcb pointer to heap allocation
 void PCB_destruct(PCB_p p);  // deallocates pcb from the heap
-int PCB_init(PCB_p p);       // sets default values for member data
-int PCB_set_pid(PCB_p p, unsigned long pid);
-int PCB_set_state(PCB_p p, enum state_type state);
-int PCB_set_priority(PCB_p p, unsigned short priority);
-int PCB_set_pc(PCB_p p, unsigned long pc);
+void PCB_init(PCB_p p);       // sets default values for member data
+void PCB_set_pid(PCB_p p, unsigned long pid);
+void PCB_set_state(PCB_p p, enum state_type state);
+void PCB_set_priority(PCB_p p, unsigned short priority);
+void PCB_set_pc(PCB_p p, unsigned long pc);
 unsigned long PCB_get_pid(PCB_p p);  // returns pid of the process
 enum state_type PCB_get_state(PCB_p p);
 unsigned short PCB_get_priority(PCB_p p);
 unsigned long PCB_get_pc(PCB_p p);
 
-int PCB_toString(PCB_p p, char *);  // returns a string representing the contents of the pcb
+void PCB_toString(PCB_p p, char *s);  // returns a string representing the contents of the pcb
 
 #endif // PCB_H_INCLUDED
