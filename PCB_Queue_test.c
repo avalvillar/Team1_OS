9f7689cf7a053984f0a68b11c7c956554762e11b
hunterwb <hunterwb@washington.edu>
2016-04-06 21:20:52 -0700

    q fix

--- PCB_Queue_test.c
+++ PCB_Queue_test.c
@@ -3,51 +3,46 @@
 #include <stdio.h>
 
 int main() {
-	// printf("\n elements \n");
-	// PCB_p p1 = PCB_construct();
-	// PCB_init(p1);
-	// char string[100];
-	// //PCB_toString(p1, string);
-	// printf("%s\n", string);
+	enum PCB_ERROR error = PCB_SUCCESS;
+	printf("\n elements \n");
+	PCB_p p1 = PCB_construct(&error);
+	PCB_init(p1, &error);
+	// PCB_print(p1, &error);
 
-	// PCB_set_pid(p1, 64);
-	// PCB_set_state(p1, PCB_STATE_READY);
-	// PCB_set_priority(p1, 8);
-	// PCB_set_pc(p1, 128);
-	// PCB_toString(p1, string);
-	// printf("%s\n", string);
+	PCB_set_pid(p1, 64, &error);
+	PCB_set_state(p1, PCB_STATE_READY, &error);
+	PCB_set_priority(p1, 8, &error);
+	PCB_set_pc(p1, 128, &error);
+	// PCB_print(p1, &error);
 	
-	// //second p2
-	// PCB_p p2 = PCB_construct();
-	// PCB_init(p2);
-	// //PCB_toString(p2, string);
-	// printf("%s\n", string);
+	//second p2
+	PCB_p p2 = PCB_construct(&error);
+	PCB_init(p2, &error);
+	// PCB_print(p2, &error);
 
-	// PCB_set_pid(p2, 65);
-	// PCB_set_state(p2, PCB_STATE_RUNNING);
-	// PCB_set_priority(p2, 7);
-	// PCB_set_pc(p2, 118);
-	// PCB_toString(p2, string);
-	// printf("%s\n", string);
+	PCB_set_pid(p2, 65, &error);
+	PCB_set_state(p2, PCB_STATE_RUNNING, &error);
+	PCB_set_priority(p2, 7, &error);
+	PCB_set_pc(p2, 118, &error);
+	// PCB_print(p2, &error);
 
-	// PCB_Queue_p list = PCB_Queue_construct();
-	// PCB_Queue_enqueue(list,p1);
-	// PCB_Queue_enqueue(list,p2);
+	PCB_Queue_p list = PCB_Queue_construct(&error);
+	PCB_Queue_enqueue(list,p1, &error);
+	PCB_Queue_enqueue(list,p2, &error);
 	
-	// printf("\nTest for enqueue \n");
-	// PCB_Queue_toString(list);	
+	printf("\nTest for enqueue \n");
+	PCB_Queue_print(list, &error);	
 
-	// printf("\nTest for denqueue \n");
-	// PCB_p pcbp = PCB_Queue_dequeue(list);
-	// PCB_Queue_toString(list);	
+	printf("\nTest for denqueue \n");
+	PCB_p pcbp = PCB_Queue_dequeue(list, &error);
+	PCB_Queue_print(list, &error);	
 	
-	// printf("\nTest for denqueue values \n");
-	// PCB_toString(pcbp, string);
-	// printf("%s\n", string);
+	printf("\nTest for denqueue values \n");
+	PCB_print(pcbp, &error);
 
 	
-	// PCB_destruct(p1);
-	// PCB_destruct(p2);
+	PCB_destruct(p1, &error);
+	PCB_destruct(p2, &error);
 
 
 	return 0;
